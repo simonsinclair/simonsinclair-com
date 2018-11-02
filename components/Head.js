9ccd9ed115b61a223e9f7a7eb1c5f3d01feba1dc
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-11-02 13:59:47 +0000

    Add components/Head.js

---
+++ components/Head.js
@@ -0,0 +1,12 @@
+import NextHead from 'next/head';
+
+const Head = () => (
+  <NextHead>
+    <meta charSet="UTF-8" />
+    <title>Simon Sinclair &mdash; Home</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </NextHead>
+);
+
+export default Head;
