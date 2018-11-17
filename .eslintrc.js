b823fe8a41b179c988797c412bdce4ee11530333
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-11-17 16:40:44 +0000

    Disable some ESLint rules
    To play nicely with Next JS.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   parser: "babel-eslint",
   extends: 'airbnb',
+  rules: {
+    // In a Next JS project, React
+    // doesn't have to be in-scope.
+    "react/react-in-jsx-scope": 0,
+
+    // Next JS's <Link /> component
+    // requires an <a /> as its child.
+    "jsx-a11y/anchor-is-valid": 0,
+  },
 };
