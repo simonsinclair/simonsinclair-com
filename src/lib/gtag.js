d9fac33e15e1d003cbf82acda73198c93081fce2
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-05-29 18:42:38 +0100

    Catch lib/gtag.js errors

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,18 +2,26 @@ export const GA_TRACKING_ID = 'UA-128283216-1';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
+  try {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  } catch (err) {
+    console.error('gtag pageview error', err);
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({
   action, category, label, value,
 }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
+  try {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  } catch (err) {
+    console.error('gtag event error', err);
+  }
 };
