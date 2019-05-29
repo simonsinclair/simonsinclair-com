5e5fb6fcea48f49328a34ad03b85dc85367c7ddc
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-05-29 18:45:13 +0100

    Update _error.jsx

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -3,12 +3,8 @@ import Layout from '../components/Layout';
 
 const Error = () => (
   <Layout title="Simon Sinclair &mdash; Not Found">
-    <h1 className="logotype">404</h1>
-    <style jsx>
-      {`
-        .logotype {}
-      `}
-    </style>
+    <h1>Not Found</h1>
+    <p>Sorry, that page couldn&apos;t be found.</p>
   </Layout>
 );
 
