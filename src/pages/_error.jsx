00ba5c3e1652a22b8f131479f42bde5439dd1427
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-05-31 00:30:02 +0100

    Update _error.jsx

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -3,8 +3,10 @@ import Page from '../components/Page';
 
 const Error = () => (
   <Page title="Simon Sinclair &mdash; Page Not Found">
-    <h1>Page Not Found</h1>
-    <p>Sorry, that page could not be found.</p>
+    <div className="container">
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you&apos;re trying to view cannot be found.</p>
+    </div>
   </Page>
 );
 
