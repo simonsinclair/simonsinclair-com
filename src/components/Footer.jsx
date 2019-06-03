cdaf250c6bd2c910a338cde1d8326db0f6db5ce0
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-06-04 00:31:51 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,21 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-// import * as gtag from '../lib/gtag';
+const Footer = () => (
+  <footer className="container">
+    <p>
+      <small>
+        &copy;
+        {' '}
+        {new Date().getFullYear()}
+        {' '}
+        Simon Sinclair.
+      </small>
+    </p>
 
-const Footer = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-
-  const handleClickExternal = (e) => {
-    e.preventDefault();
-    // gtag.event({
-    //   action: 'submit_form',
-    //   category: 'Contact',
-    //   label: this.state.message
-    // });
-    console.log('To do: gtag.event.');
-  };
-
-  return (
-    <footer className="container">
-      <p><small>&copy; {year} Simon Sinclair. <a onClick={handleClickExternal} href="https://github.com/simonsinclair/simonsinclair-com">Source</a>.</small></p>
-
-      <style jsx>
-        {`
-          footer {}
-        `}
-      </style>
-    </footer>
-  );
-};
+    <style jsx>
+      {`
+        footer {}
+      `}
+    </style>
+  </footer>
+);
 
 export default Footer;
