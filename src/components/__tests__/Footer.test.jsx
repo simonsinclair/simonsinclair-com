84549081aa7683dab0b85289bb7f306a9cd1117f
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-07-08 00:59:00 +0100

    Update Footer tests

--- src/components/__tests__/Footer.test.jsx
+++ src/components/__tests__/Footer.test.jsx
@@ -4,15 +4,15 @@ import { render } from '@testing-library/react';
 
 import Footer from '../Footer';
 
-describe('<Footer />', () => {
-  it('Renders correct copyright message', () => {
+describe('<Footer /> is a component that', () => {
+  it('renders the correct copyright notice year', () => {
     const { getByText } = render(<Footer date={new Date(88, 0, 15)} />);
 
     // getByText - Get any element by its text content.
     expect(getByText('© 1988 Simon Sinclair.')).not.toBeNull();
   });
 
-  it('Should match Snapshot', () => {
+  it('matches its snapshot', () => {
     // asFragment - Returns a DocumentFragment of your rendered component.
     const { asFragment } = render(<Footer date={new Date(88, 0, 15)} />);
     expect(asFragment()).toMatchSnapshot();
