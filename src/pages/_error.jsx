e875a8aac7035c2932ffd9d9477fcb2c60b697f2
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-08-07 18:56:26 +0100

    Add meta:description to _error page

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Page from '../components/Page/Page';
 
 const Error = () => (
-  <Page title="Simon Sinclair &mdash; Oops">
+  <Page title="Simon Sinclair &mdash; Oops" description="The page you requested could not be found.">
     <main className="container">
       <h1>Oops</h1>
       <p>Sorry, that page cannot be found.</p>
