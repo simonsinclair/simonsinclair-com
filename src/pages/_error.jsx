f19568cdd7354dec68c440edd24a1ca2120ebe8f
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-08-07 18:56:35 +0100

    Update _error page copy

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -5,7 +5,7 @@ const Error = () => (
   <Page title="Simon Sinclair &mdash; Oops" description="The page you requested could not be found.">
     <main className="container">
       <h1>Oops</h1>
-      <p>Sorry, that page cannot be found.</p>
+      <p>Sorry, the page you requested could not be found.</p>
     </main>
   </Page>
 );
