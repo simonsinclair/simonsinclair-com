80b95d9749e87b40633eadf21b537cc4df27b518
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-08-10 12:30:19 +0100

    Optimise custom _document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,12 @@
+// '_document' is only rendered on the server side and not on the client
+// side. Event handlers like `onClick` can't be added to this file.
+// ---------------------------------------------------------------------------------
+// Note: React-components outside of <Main /> will not be initialised by the browser
+// browser. Do not add application logic here. If you need shared components in all
+// your pages (like a menu or a toolbar), take a look at the <App> component instead.
+
 /* eslint-disable react/no-danger */
+import React from 'react';
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
@@ -7,27 +15,15 @@ import { ServerStyleSheet } from 'styled-components';
 import { GA_TRACKING_ID } from '../lib/gtag';
 
 class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-      });
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    const page = renderPage({
+      enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+    });
+    return {
+      ...page,
+      styles: sheet.getStyleElement(),
+    };
   }
 
   render() {
