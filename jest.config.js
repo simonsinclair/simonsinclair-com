b6fd19075356c08dba378acdb97016286d22ad08
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-08-14 18:43:04 +0100

    Add /node_modules/ and /public/ to Jest test ignore

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
   verbose: true,
 
   setupFilesAfterEnv: [
-    // Ensure 'cleanup' is called after each test.
-    // https://testing-library.com/docs/react-testing-library/api#cleanup
     '@testing-library/react/cleanup-after-each',
     'jest-styled-components',
   ],
@@ -11,10 +9,7 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>/.firebase/',
     '<rootDir>/build/',
-    '<rootDir>/bundles/',
-
-    // We require node_modules in test
-    // for things like 'normalize.css'.
-    // '<rootDir>/node_modules/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/public/',
   ],
 };
