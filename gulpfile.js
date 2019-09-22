fe49cb8aa380ddb059a39484c47ff14d669f1aee
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-22 19:19:33 +0100

    Update SASS build location

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ sass.compiler = require('node-sass');
 
 gulp.task('sass', () => gulp.src('./src/sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./src/site/_includes')));
+  .pipe(gulp.dest('./src/site/_includes/css')));
 
 gulp.task('sass:watch', () => {
   gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
