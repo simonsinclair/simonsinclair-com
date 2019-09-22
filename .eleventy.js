b426d95001273fa0874a7e48301c0dc4c4d2d87f
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-22 19:13:12 +0100

    Uninstall clean-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,3 @@
-const CleanCSS = require('clean-css');
 const Terser = require('terser');
 
 module.exports = function(config) {
@@ -8,12 +7,6 @@ module.exports = function(config) {
   config.addPassthroughCopy('src/site/favicon.ico');
   config.addPassthroughCopy('src/site/robots.txt');
   config.addPassthroughCopy('src/site/sitemap.xml');
-
-  config.addFilter('cssmin', function(code) {
-    return new CleanCSS({})
-      .minify(code)
-      .styles;
-  });
 
   config.addFilter('jsmin', function(code) {
     const minified = Terser.minify(code);
