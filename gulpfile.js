a64a9210f1461a9dabe0f45beead45c1618220f4
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-23 21:50:32 +0100

    Compress SASS output

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', () => gulp.src('./src/sass/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
+  .pipe(
+    sass({ outputStyle: 'compressed' })
+      .on('error', sass.logError),
+  )
   .pipe(gulp.dest('./src/site/_includes/css')));
 
 gulp.task('sass:watch', () => {
