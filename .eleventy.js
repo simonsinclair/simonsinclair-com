9a5b8e257622d74ead07c3b753729f6058891ab8
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-28 23:35:21 +0100

    Remove empty config. object from CleanCSS

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
   config.addPassthroughCopy('src/site/sitemap.xml');
 
   config.addFilter('cssmin', function(code) {
-    return new CleanCSS({})
+    return new CleanCSS()
       .minify(code)
       .styles;
   });
