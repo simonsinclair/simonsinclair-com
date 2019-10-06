ec52909eec05cdd9772eb4d19e760dde713b8948
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-10-06 13:07:01 +0100

    Add e2e test to check a11y

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,14 @@
+beforeEach(() => {
+  cy.visit('/');
+  cy.injectAxe();
+});
+
 describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/');
+  it('has "Simon Sinclair" in the title', () => {
+    cy.title().should('contain', 'Simon Sinclair');
+  });
+
+  it('passes a11y tests', () => {
+    cy.checkA11y();
   });
 });
