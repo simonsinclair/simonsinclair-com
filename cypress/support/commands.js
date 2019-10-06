3ace92417cbce3c4732c31b178f3eaf536bce14e
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-10-06 13:05:49 +0100

    Install cypress-axe

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('cypress-axe');
