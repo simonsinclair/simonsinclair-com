8d4959c58429db96843af82522c52daa4bd6dfa1
Simon Sinclair <simon@simonsinclair.com>
2020-10-04 16:14:37 +0100

    Set year data dynamically

---
+++ src/site/_data/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'Simon Sinclair — Home',
+  description: 'Simon Sinclair is a Software Engineer from the United Kingdom.',
+  year: new Date().getFullYear(),
+  ga_tracking_id: 'UA-128283216-1',
+};
