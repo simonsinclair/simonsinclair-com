afce50e8bb4430507fd35198dd95a703b9961015
Simon Sinclair <simon@simonsinclair.com>
2021-12-18 15:36:46 +0000

    V2

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # simonsinclair.com
-
-A static site built using Eleventy.
-
-## Overview
-```
-/src
-  /site - processed by Eleventy.
-    /_data - global data files.
-    /_includes - files included by Nunjucks templates.
-      /css
-      /js
-      /layouts
-      /partials
-    /assets - static assets, which are copied to /public/assets.
-```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,7 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ['./src/**/*.njk'],
+  content: ['./public/**/*.html'],
   theme: {
-    colors: {
-      neutral: 'var(--color-neutral)',
-      primary: 'var(--color-primary)',
-    },
-    fontFamily: {
-      sans: ['IBM Plex Sans', 'Helvetica Neue', 'Arial', 'sans-serif'],
-      mono: [
-        'IBM Plex Mono',
-        'Menlo',
-        'DejaVu Sans Mono',
-        'Bitstream Vera Sans Mono',
-        'Courier',
-        'monospace',
-      ],
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-    },
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
